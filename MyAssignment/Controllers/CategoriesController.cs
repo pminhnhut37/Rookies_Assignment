1331a125bd258abd7e6b61f0e53467e4b5d8619a
nhut <pminhnhut37@gmail.com>
2021-04-02 15:29:14 +0700

    Add permission (Additional)

--- MyAssignment/Controllers/CategoriesController.cs
+++ MyAssignment/Controllers/CategoriesController.cs
@@ -47,6 +47,7 @@ namespace MyAssignment.Controllers
         // PUT: api/Categories/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}")]
+        [Authorize(Roles = "admin")]
         public async Task<IActionResult> PutCategory(int id, Category category)
         {
             if (id != category.IDCate)
@@ -78,6 +79,7 @@ namespace MyAssignment.Controllers
         // POST: api/Categories
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
+        [Authorize(Roles = "admin")]
         public async Task<ActionResult<Category>> PostCategory(Category category)
         {
             _context.Categories.Add(category);
@@ -88,6 +90,7 @@ namespace MyAssignment.Controllers
 
         // DELETE: api/Categories/5
         [HttpDelete("{id}")]
+        [Authorize(Roles = "admin")]
         public async Task<IActionResult> DeleteCategory(int id)
         {
             var category = await _context.Categories.FindAsync(id);
